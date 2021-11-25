85358313f941d252a302abbcad520329d2316947
mani3710 <mani37105@gmail.com>
2021-11-25 13:00:35 +0530

    navigation is added

---
+++ src/navigation/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+import Home from '../screen/home';
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
