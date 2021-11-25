f617ec4cc82a6496eac5ac655b9be34ce4370cb5
aravinthraj98 <aravinth799902@gmail.com>
2021-11-25 10:45:20 +0530

    firebase queries

--- App.js
+++ App.js
@@ -6,37 +6,44 @@
  * @flow strict-local
  */
 
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 
-import {
-  SafeAreaView,
-  Text,
-  
+import {SafeAreaView, Text} from 'react-native';
 
-} from 'react-native';
-
-
-import firestore from "@react-native-firebase/firestore";
-
-
+import firestore from '@react-native-firebase/firestore';
 
 const App = () => {
- useEffect(()=>{
-       async function data(){
-        //  await firestore().collection("user").doc("userName").set({name:"aravinth"});
-        //  await firestore().collection("user").doc("userName").update({name:"mugilan"});
-        //  console.log("Hello");
-       }
-       data();
- },[])
+  useEffect(() => {
+    async function data() {
+      // await firestore()
+      //   .collection('user')
+      //   .doc('userName')
+      //   .set({name: 'aravinth', age: 22}); // replace
+      // await firestore()
+      //   .collection('user')
+      //   .doc('userName')
+      //   .update({name: 'mugilan'});//update field
+      // await firestore()
+      //   .collection('user')
+      //   .get()
+      //   .then(snap => {
+      //     snap.forEach(doc => console.log(doc.id + ' ' + doc.data().name));
+      //   }); //get
+
+      //  await firestore().collection("user").doc("userName").delete();//delete
+
+      //  console.log("Hello");
+      let data = new Date();
+      data.setDate(new Date(data) + 1);
+    }
+    data();
+  }, []);
 
   return (
-    <SafeAreaView >
-     <Text>Hello</Text>
+    <SafeAreaView>
+      <Text>Hello</Text>
     </SafeAreaView>
   );
 };
-
-
 
 export default App;
